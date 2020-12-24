d1fbf1b20e56806f73e338d5f10a1d974545769d
redpandaathome <leeyumi415@gmail.com>
2020-12-24 12:25:05 +0900

    query/body decorators

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post, Put, Body, Query } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -8,14 +8,20 @@ export class MoviesController {
       return "This will return all movies"
    }
 
+   @Get("search")
+   search(@Query('year') searchingYear: string){
+      return `We are searching...${searchingYear}`
+   }
+
    @Get("/:id")
    getOne(@Param('id') movieId: string){
       return `This will return one movie with movieId ${movieId}`
    }
 
    @Post()
-   create(){
-      return 'This will create a movie'
+   create(@Body() movieData){
+      console.log(movieData)
+      return movieData
    }
 
    @Delete("/:id")
@@ -25,7 +31,10 @@ export class MoviesController {
    }
 
    @Patch("/:id")
-   patch(@Param('id') movieId: string){
-      return `This will patch one movie with movieId ${movieId}`
+   patch(@Param('id') movieId: string, @Body() updateData){
+      return {
+         updatedMovie: movieId,
+         ...updateData
+      }
    }
 }
